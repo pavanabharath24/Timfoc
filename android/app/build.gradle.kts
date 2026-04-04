468a5a06563eb58a344f1d0e1f580dad012c8c66
pavanabharath24 <korukondapavanbharath@gmail.com>
2026-04-05 02:14:34 +0530

    build: lock compileSdk and targetSdk to 36 natively for robust Github/Google Play CI compilation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.timfoc.timfoc"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -25,8 +25,8 @@ android {
         applicationId = "com.timfoc.timfoc"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
